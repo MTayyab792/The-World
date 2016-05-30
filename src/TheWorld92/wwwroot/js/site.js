11969af0e3bb2a594a056159e908fc09239ac38d
MTayyab792 <mtayyab792@gmail.com>
2016-05-30 20:29:20 +0500

    jquery add index

---
+++ src/TheWorld92/wwwroot/js/site.js
@@ -0,0 +1,39 @@
+﻿
+//function startup() {
+//    var ele = document.getElementById("username");
+//    ele.innerHTML = "web Develpoer";
+
+//    var main = document.getElementById("main");
+//    main.onmouseenter = function () {
+
+//        main.style["background-color"] = "#888;";
+//    };
+//    main.onmouseleave = function () {
+
+//        main.style["background-color"] = "";
+
+//    };
+//};
+//startup();
+
+(function() {
+    var ele = $("#username");
+    ele.text("web Developer");
+    var main = $("#main");
+    main.on("mouseenter", function () {
+
+        main.style="background-color:black;";
+    });
+    main.on("mouseleave", function () {
+
+        main.style ="";
+
+    });
+
+    var menuitem = $("ul.menu li a");
+    menuitem.on("click", function () {
+
+        alert("Hello")
+    })
+
+})();
