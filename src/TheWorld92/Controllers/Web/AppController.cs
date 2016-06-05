108c813614dc476c1f90efd623f5f118530e1a3b
MTayyab792 <mtayyab792@gmail.com>
2016-06-06 00:08:25 +0500

    Email Service and Validation RenderSection with jquery and unobstrctive

--- src/TheWorld92/Controllers/Web/AppController.cs
+++ src/TheWorld92/Controllers/Web/AppController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using TheWorld92.ViewModels;
+using TheWorld92.Services;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,7 +12,11 @@ namespace TheWorld92.Controllers.Web
 {
     public class AppController : Controller
     {
-        // GET: /<controller>/
+        private IMailService _mailService;
+        public AppController(IMailService service)
+        {
+            _mailService = service;
+        }
         public IActionResult Index()
         {
             return View();
@@ -21,6 +27,27 @@ namespace TheWorld92.Controllers.Web
         }
         public IActionResult Contact()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid) { 
+            var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+                if (string.IsNullOrWhiteSpace(email))
+                {
+                    ModelState.AddModelError("", "Could not send email, configration problem");
+                }
+
+                if(_mailService.SendMail("email",
+                "email",
+                $"Contact Page from {model.Name} ({model.Email})",
+                model.Message))
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = "Mail Sent. Thanks!"; 
+                }
+            }
             return View();
         }
     }
